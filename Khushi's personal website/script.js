a4e4a6a0f2dd697927f3fd68f317078479e9944c
khushiR8 <beedassykhushi@gmail.com>
2025-07-31 16:20:42 +0400

    modified uni projects

--- Khushi's personal website/script.js
+++ Khushi's personal website/script.js
@@ -12,43 +12,8 @@ function scrollToElement(elementSelector, instance = 0) {
   }
 }
 
-document.getElementById("link3").addEventListener("click", function () {
+document.getElementById("link1").addEventListener("click", function () {
     window.location.href = "index.html";})
-
-// const link1 = document.getElementById("link1");
-// const link2 = document.getElementById("link2");
-
-// link1.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   scrollToElement('.skill-container');
-// });
-
-// link2.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   scrollToElement('.content-para');
-// });
-
-// window.addEventListener('scroll', function() {
-//   const header = document.querySelector('.header-container');
-//   if (window.scrollY > 50) { // Change 50 to the scroll threshold you want
-//       header.classList.add('scrolled');
-//   } else {
-//       header.classList.remove('scrolled');
-//   }
-// });
-
-// function checkScroll() {
-//   var header = document.querySelector('.header-container');
-//   if (window.scrollY > 0) {
-//       header.classList.add('scrolled');
-//   } else {
-//       header.classList.remove('scrolled');
-//   }
-// }
-
-// window.addEventListener('load', checkScroll);
-// window.addEventListener('scroll', checkScroll);
-
 
 document.addEventListener('DOMContentLoaded', function() {
   // Function to check scroll position and toggle 'scrolled' class
